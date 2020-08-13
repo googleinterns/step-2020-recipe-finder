13fc7a18c4d3eda104bf649b20581cab7c2bfbc2
Mohamed <mohamed.malem0@gmail.com>
2020-08-13 17:25:28 +0100

    create text input page

--- frontend/src/components/InputText.js
+++ frontend/src/components/InputText.js
@@ -1,8 +1,64 @@
 import React, {Component} from 'react';
+import InputTextItems from './InputTextItems'
+import './InputText.css'
 
 class InputText extends Component {
-    render() {
-        return (<h1>Text</h1>)
+    constructor(props) {
+        super(props);
+       
+        this.state = {
+          items: []
+        };
+       
+        this.addItem = this.addItem.bind(this);
+        this.deleteItem = this.deleteItem.bind(this);
+
+      }
+
+    addItem(e) {
+        if (this._inputElement.value !== "") {
+          var newItem = {
+            text: this._inputElement.value,
+            key: Date.now()
+          };
+       
+          this.setState((prevState) => {
+            return { 
+              items: prevState.items.concat(newItem) 
+            };
+          });
+         
+          this._inputElement.value = "";
+        }
+         
+        console.log(this.state.items);
+           
+        e.preventDefault();
     }
-}
+    deleteItem(key) {
+        var filteredItems = this.state.items.filter(function (item) {
+          return (item.key !== key);
+        });
+       
+        this.setState({
+          items: filteredItems
+        });
+    }
+    
+    render() {
+        return (
+          <div className="todoListMain">
+            <div className="header">
+              <h1>Input Ingredients</h1>
+              <form onSubmit={this.addItem}>
+              <input ref={(a) => this._inputElement = a} placeholder="New Ingredient">
+                </input>
+                <button type="submit">add</button>
+              </form>
+            </div>
+            <InputTextItems entries={this.state.items} delete={this.deleteItem}/>
+          </div>
+        );
+      }
+    }
 export default InputText;
