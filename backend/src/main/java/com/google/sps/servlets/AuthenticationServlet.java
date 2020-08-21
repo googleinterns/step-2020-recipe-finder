67e81e8da3bea4ac227865dc9919b40803dde936
Azhara Assanova <azhara.assanova@gmail.com>
2020-08-21 15:52:33 +0000

    Format code

--- backend/src/main/java/com/google/sps/servlets/AuthenticationServlet.java
+++ backend/src/main/java/com/google/sps/servlets/AuthenticationServlet.java
@@ -1,3 +1,17 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package com.google.sps.servlets;
 
 import com.google.appengine.api.users.UserService;
@@ -15,7 +29,7 @@ public abstract class AuthenticationServlet extends HttpServlet {
     UserService userService = UserServiceFactory.getUserService();
     if (!userService.isUserLoggedIn()) {
       response.setStatus(AUTHENTICATION_ERROR_CODE);
-      return;    
+      return;
     }
     get(request, response);
   }
@@ -25,12 +39,14 @@ public abstract class AuthenticationServlet extends HttpServlet {
     UserService userService = UserServiceFactory.getUserService();
     if (!userService.isUserLoggedIn()) {
       response.setStatus(AUTHENTICATION_ERROR_CODE);
-      return;    
+      return;
     }
     post(request, response);
   }
 
-  protected abstract void post(HttpServletRequest request, HttpServletResponse response) throws IOException;
-  protected abstract void get(HttpServletRequest request, HttpServletResponse response) throws IOException;
+  protected abstract void post(HttpServletRequest request, HttpServletResponse response)
+      throws IOException;
 
+  protected abstract void get(HttpServletRequest request, HttpServletResponse response)
+      throws IOException;
 }
--- backend/src/main/java/com/google/sps/servlets/FindRecipesServlet.java
+++ backend/src/main/java/com/google/sps/servlets/FindRecipesServlet.java
@@ -20,7 +20,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
