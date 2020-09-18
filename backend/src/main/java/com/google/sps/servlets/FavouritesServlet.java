ed906814d494599f1ff9c1369507ab0f47853f5f
Azhara Assanova <azhara.assanova@gmail.com>
2020-09-18 14:13:19 +0000

    Remove user service imports

--- backend/src/main/java/com/google/sps/servlets/FavouritesServlet.java
+++ backend/src/main/java/com/google/sps/servlets/FavouritesServlet.java
@@ -15,7 +15,6 @@
 package com.google.sps.servlets;
 
 import com.google.appengine.api.datastore.Entity;
-import com.google.appengine.api.users.UserService;
 import com.google.gson.Gson;
 import com.google.sps.data.Recipe;
 import com.google.sps.utils.DatastoreUtils;
--- backend/src/main/java/com/google/sps/servlets/RemoveFavouriteServlet.java
+++ backend/src/main/java/com/google/sps/servlets/RemoveFavouriteServlet.java
@@ -17,7 +17,6 @@ package com.google.sps.servlets;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
-import com.google.appengine.api.users.UserService;
 import com.google.sps.utils.DatastoreUtils;
 import com.google.sps.utils.UserConstants;
 import java.io.IOException;
